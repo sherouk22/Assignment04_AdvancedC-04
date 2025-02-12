82c3afe8b9672984304766d7bfab2723147b8c8e
sherouk22 <sheroukmohy558@gmail.com>
2025-02-12 10:14:07 +0200

    Add project files.

---
+++ Assignment04_AdvancedC-04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment04_AdvancedC_04
+{
+    internal class Program
+    { //Assignment03_AdvancedC-03
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
